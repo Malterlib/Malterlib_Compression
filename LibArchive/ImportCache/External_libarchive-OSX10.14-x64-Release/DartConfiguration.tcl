35ff823deacc519415d63db444e9afe0f8a26b20
Erik Olofsson <erik@olofsson.info>
2021-11-09 11:40:15 +0100

    [Malterlib/Compression] Update import cache

--- LibArchive/ImportCache/External_libarchive-OSX10.14-x64-Release/DartConfiguration.tcl
+++ LibArchive/ImportCache/External_libarchive-OSX10.14-x64-Release/DartConfiguration.tcl
@@ -62,7 +62,7 @@ UpdateOptions:
 UpdateType: git
 
 # Compiler info
-Compiler: /opt/CompiledFiles/Dependencies/llvm/11.0/build/main/bin/clang++
+Compiler: /opt/CompiledFiles/Dependencies/llvm/12.0/build/main/bin/clang++
 CompilerVersion: 
 
 # Dynamic analysis (MemCheck)
