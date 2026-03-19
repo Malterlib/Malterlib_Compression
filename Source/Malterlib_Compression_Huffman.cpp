0ac76945a00b62a48da32dea5117f25233188f1d
Erik Olofsson <erik@olofsson.info>
2026-03-19 23:16:27 +0100

    Clean up huffman implementation

---
+++ Source/Malterlib_Compression_Huffman.cpp
@@ -0,0 +1,327 @@
+// Copyright © 2026 Unbroken AB
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include <Mib/Core/Core>
+#include "Malterlib_Compression_Huffman.h"
+
+namespace NMib::NCompression
+{
+	// Serialized node size: uint32 frequency + uint8 ascii
+	namespace
+	{
+		static constexpr umint gc_NodeSerializedSize = sizeof(uint32) + sizeof(uint8);
+
+		struct CHuffmanNode
+		{
+			CHuffmanNode() = default;
+
+			CHuffmanNode *m_pParent = nullptr;
+			CHuffmanNode *m_pLeft = nullptr;
+			CHuffmanNode *m_pRight = nullptr;
+
+			uint64 m_Code = 0;
+			uint32 m_Frequency = 0;
+			int32 m_CodeLength = 0;
+			uint8 m_ByAscii = 0;
+		};
+
+		struct CSort_FrequencyCompare
+		{
+			COrdering_Partial operator()(CHuffmanNode &_Elem1, CHuffmanNode &_Elem2) const;
+		};
+
+		struct CSort_ASCIICompare
+		{
+			COrdering_Partial operator()(CHuffmanNode &_Elem1, CHuffmanNode &_Elem2) const;
+		};
+
+		auto fg_PopNode(CHuffmanNode *_pNodes[], umint _iIndex, bool _bRight) -> CHuffmanNode *
+		{
+			CHuffmanNode *pNode = _pNodes[_iIndex];
+			pNode->m_Code = _bRight;
+			pNode->m_CodeLength = 1;
+			return pNode;
+		}
+
+		void fg_SetNodeCode(CHuffmanNode *_pNode)
+		{
+			CHuffmanNode *pParent = _pNode->m_pParent;
+			while (pParent && pParent->m_CodeLength)
+			{
+				_pNode->m_Code <<= 1;
+				_pNode->m_Code |= pParent->m_Code;
+				_pNode->m_CodeLength++;
+				pParent = pParent->m_pParent;
+			}
+		}
+
+		umint fg_GetHuffmanTree(CHuffmanNode _Nodes[], bool _bSetCodes)
+		{
+			CHuffmanNode *pNodes[256];
+			CHuffmanNode *pNode;
+
+			// add used ascii to Huffman queue
+			umint nNodes = 0;
+			for (umint iCount = 0; iCount < 256 && _Nodes[iCount].m_Frequency; iCount++)
+				pNodes[nNodes++] = &_Nodes[iCount];
+			umint iParentNode = nNodes;
+			smint iBackNode = smint(nNodes) - 1;
+
+			while (iBackNode > 0)
+			{
+				// parent node
+				pNode = &_Nodes[iParentNode++];
+				// pop first child
+				pNode->m_pLeft = fg_PopNode(pNodes, iBackNode--, false);
+				// pop second child
+				pNode->m_pRight = fg_PopNode(pNodes, iBackNode--, true);
+				// adjust parent of the two popped nodes
+				pNode->m_pLeft->m_pParent = pNode->m_pRight->m_pParent = pNode;
+				// adjust parent frequency
+				pNode->m_Frequency = pNode->m_pLeft->m_Frequency + pNode->m_pRight->m_Frequency;
+				// insert parent node depending on its frequency
+				smint i;
+				for (i = iBackNode; i >= 0; i--)
+					if (pNodes[i]->m_Frequency >= pNode->m_Frequency)
+						break;
+				NMemory::fg_MemMove(pNodes + (i + 2), pNodes + (i + 1), (iBackNode - i) * sizeof(CHuffmanNode *));
+				pNodes[i + 1] = pNode;
+				iBackNode++;
+			}
+			// set tree leaves nodes code
+			if (_bSetCodes)
+				for (umint iCount = 0; iCount < nNodes; iCount++)
+					fg_SetNodeCode(&_Nodes[iCount]);
+
+			return nNodes;
+		}
+
+		COrdering_Partial CSort_FrequencyCompare::operator()(CHuffmanNode &_Elem1, CHuffmanNode &_Elem2) const
+		{
+			return _Elem2.m_Frequency <=> _Elem1.m_Frequency;
+		}
+
+		COrdering_Partial CSort_ASCIICompare::operator()(CHuffmanNode &_Elem1, CHuffmanNode &_Elem2) const
+		{
+			return _Elem1.m_ByAscii <=> _Elem2.m_ByAscii;
+		}
+
+		auto fg_GetCompressedAllocSize(umint _nHeaderSize, umint _nSrcLen) -> umint
+		{
+			uint64 nAllocSize = uint64(_nHeaderSize) + uint64(_nSrcLen) + sizeof(uint64);
+			if (nAllocSize > TCLimitsInt<umint>::mc_Max)
+				DMibError("Huffman compression output exceeds maximum buffer length");
+			return (umint)nAllocSize;
+		}
+
+		auto fg_GetEncodedBitCount(CHuffmanNode const _Nodes[], umint _nNodes) -> uint64
+		{
+			uint64 nEncodedBits = 0;
+			for (umint iCount = 0; iCount < _nNodes; iCount++)
+				nEncodedBits += uint64(_Nodes[iCount].m_Frequency) * uint64(_Nodes[iCount].m_CodeLength);
+			return nEncodedBits;
+		}
+	}
+
+	NContainer::CByteVector fg_CompressHuffman(NContainer::CByteVector const &_Source)
+	{
+		uint8 const *pSrc = _Source.f_GetArray();
+		umint nSrcLen = _Source.f_GetLen();
+
+		if (nSrcLen > TCLimitsInt<uint32>::mc_Max)
+			DMibError("Huffman compression input exceeds uint32 maximum length");
+
+		if (nSrcLen == 0)
+		{
+			NContainer::CByteVector Destination;
+			Destination.f_SetLen(sizeof(uint32));
+			uint32 Zero = 0;
+			NMemory::fg_MemCopy(Destination.f_GetArray(), &Zero, sizeof(Zero));
+			return Destination;
+		}
+
+		CHuffmanNode Nodes[511];
+		// initialize nodes ascii
+		for (umint iCount = 0; iCount < 256; iCount++)
+			Nodes[iCount].m_ByAscii = iCount;
+
+		// get ascii frequencies
+		for (umint iCount = 0; iCount < nSrcLen; iCount++)
+			Nodes[pSrc[iCount]].m_Frequency++;
+		// sort ascii chars depending on frequency
+
+		NMemory::fg_QSort(Nodes, 256, CSort_FrequencyCompare());
+
+		// construct Huffman tree
+		umint nNodes = fg_GetHuffmanTree(Nodes, true);
+		// construct compressed buffer
+		umint nHeaderSize = sizeof(uint32) + sizeof(uint8) + nNodes * gc_NodeSerializedSize;
+		// Allocate header + worst-case bitstream (nSrcLen bytes) + sizeof(uint64) padding
+		// for the 8-byte read-modify-write used when encoding bits
+		umint nAllocSize = fg_GetCompressedAllocSize(nHeaderSize, nSrcLen);
+
+		NContainer::CByteVector Destination;
+		Destination.f_SetLen(nAllocSize);
+		uint8 *pDes = Destination.f_GetArray();
+		NMemory::fg_MemClear(pDes, nAllocSize);
+
+		uint8 *pDesPtr = pDes;
+		// save source buffer length as little-endian uint32
+		{
+			uint32 Temp = fg_ByteSwapLE((uint32)nSrcLen);
+			NMemory::fg_MemCopy(pDesPtr, &Temp, sizeof(Temp));
+		}
+		pDesPtr += sizeof(uint32);
+		// save Huffman tree leaves count-1 (as it may be 256)
+		*pDesPtr = (uint8)(nNodes - 1);
+		pDesPtr += sizeof(uint8);
+		// save Huffman tree used leaves nodes
+		for (umint iCount = 0; iCount < nNodes; iCount++)
+		{
+			uint32 Freq = fg_ByteSwapLE(Nodes[iCount].m_Frequency);
+			NMemory::fg_MemCopy(pDesPtr, &Freq, sizeof(Freq));
+			pDesPtr += sizeof(uint32);
+			*pDesPtr = Nodes[iCount].m_ByAscii;
+			pDesPtr += sizeof(uint8);
+		}
+		// sort nodes depending on ascii to can index nodes with its ascii value
+		NMemory::fg_QSort(Nodes, 256, CSort_ASCIICompare());
+
+		uint64 iDes = 0;
+		// loop to write codes
+		for (umint iCount = 0; iCount < nSrcLen; iCount++)
+		{
+			uint64 Temp;
+			auto *pDestination = pDesPtr + umint(iDes >> 3);
+
+			NMemory::fg_MemCopy(&Temp, pDestination, sizeof(Temp));
+			Temp = fg_ByteSwapLE(Temp);
+			Temp |= Nodes[pSrc[iCount]].m_Code << (iDes & 7);
+			Temp = fg_ByteSwapLE(Temp);
+			NMemory::fg_MemCopy(pDestination, &Temp, sizeof(Temp));
+
+			iDes += Nodes[pSrc[iCount]].m_CodeLength;
+		}
+		// trim to actual compressed size
+		umint nFinalSize = umint(uint64(pDesPtr - pDes) + ((iDes + 7) >> 3));
+		Destination.f_SetLen(nFinalSize);
+
+		return Destination;
+	}
+
+	NContainer::CByteVector fg_DecompressHuffman(NContainer::CByteVector const &_Source, umint _nMaxDestinationLen)
+	{
+		uint8 const *pSrc = _Source.f_GetArray();
+		umint nSrcLen = _Source.f_GetLen();
+		auto fThrowMalformed = []
+			{
+				DMibError("Malformed Huffman compressed data");
+			}
+		;
+		auto fThrowTooLarge = []
+			{
+				DMibError("Huffman decompression output exceeds maximum buffer length");
+			}
+		;
+
+		if (nSrcLen < sizeof(uint32))
+			fThrowMalformed();
+
+		// read destination final length as little-endian uint32
+		uint32 nDesLenRaw;
+		NMemory::fg_MemCopy(&nDesLenRaw, pSrc, sizeof(nDesLenRaw));
+		umint nDesLen = fg_ByteSwapLE(nDesLenRaw);
+
+		if (nDesLen == 0)
+		{
+			if (nSrcLen != sizeof(uint32))
+				fThrowMalformed();
+			return {};
+		}
+
+		if (nDesLen > _nMaxDestinationLen)
+			fThrowTooLarge();
+
+		if (nSrcLen < sizeof(uint32) + sizeof(uint8))
+			fThrowMalformed();
+
+		umint nNodes = *(pSrc + sizeof(uint32)) + 1;
+		umint nHeaderSize = sizeof(uint32) + sizeof(uint8) + nNodes * gc_NodeSerializedSize;
+		if (nSrcLen < nHeaderSize)
+			fThrowMalformed();
+
+		// initialize Huffman nodes with frequency and ascii
+		CHuffmanNode Nodes[511];
+		bool bSeenAscii[256] = {};
+		umint iSrc = sizeof(uint32) + sizeof(uint8);
+		uint64 nTotalFrequency = 0;
+		for (umint iCount = 0; iCount < nNodes; iCount++)
+		{
+			uint32 Freq;
+			NMemory::fg_MemCopy(&Freq, pSrc + iSrc, sizeof(Freq));
+			auto &Node = Nodes[iCount];
+			Node.m_Frequency = fg_ByteSwapLE(Freq);
+			if (Node.m_Frequency == 0)
+				fThrowMalformed();
+			nTotalFrequency += Node.m_Frequency;
+			if (nTotalFrequency > nDesLen)
+				fThrowMalformed();
+			iSrc += sizeof(uint32);
+
+			Node.m_ByAscii = *(pSrc + iSrc);
+			if (bSeenAscii[Node.m_ByAscii])
+				fThrowMalformed();
+			bSeenAscii[Node.m_ByAscii] = true;
+			iSrc += sizeof(uint8);
+		}
+		if (nTotalFrequency != nDesLen)
+			fThrowMalformed();
+		// construct Huffman tree
+		if (fg_GetHuffmanTree(Nodes, true) != nNodes)
+			fThrowMalformed();
+		uint64 nEncodedBits = fg_GetEncodedBitCount(Nodes, nNodes);
+		uint64 nExpectedSrcBits = (uint64(iSrc) << 3) + nEncodedBits;
+		uint64 nExpectedSrcLen = uint64(iSrc) + ((nEncodedBits + 7) >> 3);
+		if (nExpectedSrcBits > (uint64(nSrcLen) << 3) || nExpectedSrcLen != nSrcLen)
+			fThrowMalformed();
+		if ((nEncodedBits & 7) != 0 && (pSrc[nSrcLen - 1] & uint8(TCLimitsInt<uint8>::mc_Max << (nEncodedBits & 7))) != 0)
+			fThrowMalformed();
+		// get Huffman tree root
+		CHuffmanNode *pRoot = &Nodes[0];
+		while (pRoot->m_pParent)
+			pRoot = pRoot->m_pParent;
+
+		NContainer::CByteVector Destination;
+		Destination.f_SetLen(nDesLen);
+		uint8 *pDes = Destination.f_GetArray();
+
+		umint iDes = 0;
+		CHuffmanNode *pNode;
+		uint64 Code;
+		uint64 nSrcBits = uint64(nSrcLen) << 3;
+		uint64 iSrcBits = uint64(iSrc) << 3;
+		while (iDes < nDesLen)
+		{
+			uint64 Temp = 0;
+			umint iSrcByte = umint(iSrcBits >> 3);
+			if (iSrcByte < nSrcLen)
+				NMemory::fg_MemCopy(&Temp, pSrc + iSrcByte, fg_Min(sizeof(Temp), nSrcLen - iSrcByte));
+			Temp = fg_ByteSwapLE(Temp);
+			Code = Temp >> (iSrcBits & 7);
+			pNode = pRoot;
+			while (pNode->m_pLeft)	// if node has pLeft then it must has pRight
+			{	// node not leaf
+				if (iSrcBits >= nSrcBits)
+					fThrowMalformed();
+				pNode = (Code & 1) ? pNode->m_pRight : pNode->m_pLeft;
+				Code >>= 1;
+				iSrcBits++;
+			}
+			pDes[iDes++] = pNode->m_ByAscii;
+		}
+		if (iSrcBits != nExpectedSrcBits)
+			fThrowMalformed();
+
+		return Destination;
+	}
+}
--- Test/Test_Malterlib_Compression_Huffman.cpp
+++ Test/Test_Malterlib_Compression_Huffman.cpp
@@ -2,6 +2,7 @@
 // Distributed under the MIT license, see license text in LICENSE.Malterlib
 
 #include <Mib/Compression/Huffman>
+#include <Mib/Test/Exception>
 
 namespace
 {
@@ -11,35 +12,294 @@ namespace
 		{
 			DMibTestSuite("Huffman")
 			{
-				NMib::NCompression::TCCompress_Huffman<> Compression;
+				using namespace NMib::NContainer;
+				using namespace NMib::NCompression;
+				using namespace NMib::NStr;
 
-				NMib::NMisc::CRandomShiftRNG Random;
-				uint8 pTestBuffer[2048];
-				for(aint i = 0; i < 2048; ++i)
+				auto fMakeByteVector = [](uint8 const *_pData, aint _nLen) -> CByteVector
+					{
+						CByteVector Vec;
+						Vec.f_SetLen(_nLen);
+						NMib::NMemory::fg_MemCopy(Vec.f_GetArray(), _pData, _nLen);
+						return Vec;
+					}
+				;
+
+				auto fTestRoundTrip = [&](uint8 const *_pData, aint _nLen)
+					{
+						CByteVector Source = fMakeByteVector(_pData, _nLen);
+						CByteVector Compressed = fg_CompressHuffman(Source);
+						CByteVector Decompressed = fg_DecompressHuffman(Compressed);
+
+						DMibExpect(Decompressed.f_GetLen(), ==, Source.f_GetLen());
+						DMibExpect(Decompressed, ==, Source)(NMib::NTest::ETestFlag_NoValues);
+					}
+				;
+
+				auto fExpectMalformedHuffman = [&](uint8 const *_pData, aint _nLen)
+					{
+						DMibExpectException
+							(
+								fg_DecompressHuffman(fMakeByteVector(_pData, _nLen))
+								, DMibErrorInstance("Malformed Huffman compressed data")
+							)
+						;
+					}
+				;
+
+				DMibTestCategory("Random")
 				{
-					pTestBuffer[i] = Random.f_GetValue<uint8>();
-				}
+					NMib::NMisc::CRandomShiftRNG Random;
+					uint8 pTestBuffer[2048];
+					for (aint i = 0; i < 2048; ++i)
+						pTestBuffer[i] = Random.f_GetValue<uint8>();
 
-				aint nDest;
-				void *pDest;
-				Compression.f_CompressHuffman(pTestBuffer,2048,pDest,nDest);
+					fTestRoundTrip(pTestBuffer, 2048);
+				};
 
-				aint nDestUncomp;
-				void *pDestUncomp;
-				Compression.f_DecompressHuffman(pDest,nDest,pDestUncomp,nDestUncomp);
-
-				uint8 * pTestFinalBuffer = (uint8*)pDestUncomp;
-				bool bDecompressDiffer = false;
-				for (aint i = 0; i < 2048; ++i)
+				DMibTestCategory("SingleByte")
 				{
-					if (pTestFinalBuffer[i] != pTestBuffer[i])
-						bDecompressDiffer = true;
-				}
+					uint8 Data[] = {42};
+					fTestRoundTrip(Data, 1);
+				};
 
-				DMibTest(DMibExpr(!bDecompressDiffer));
+				DMibTestCategory("SingleSymbol")
+				{
+					uint8 Data[1024];
+					for (aint i = 0; i < 1024; ++i)
+						Data[i] = 0xAA;
+					fTestRoundTrip(Data, 1024);
+				};
 
-				NMib::NMemory::fg_FreeNoSize(pDest);
-				NMib::NMemory::fg_FreeNoSize(pDestUncomp);
+				DMibTestCategory("TwoSymbols")
+				{
+					uint8 Data[512];
+					for (aint i = 0; i < 512; ++i)
+						Data[i] = (i & 1) ? 0xFF : 0x00;
+					fTestRoundTrip(Data, 512);
+				};
+
+				DMibTestCategory("AllByteValues")
+				{
+					uint8 Data[256];
+					for (aint i = 0; i < 256; ++i)
+						Data[i] = (uint8)i;
+					fTestRoundTrip(Data, 256);
+				};
+
+				DMibTestCategory("SkewedDistribution")
+				{
+					uint8 Data[1024];
+					for (aint i = 0; i < 1024; ++i)
+						Data[i] = (i < 900) ? 'A' : (uint8)(i & 0x1F);
+					fTestRoundTrip(Data, 1024);
+				};
+
+				DMibTestCategory("SmallSizes")
+				{
+					for (aint nSize = 2; nSize <= 16; ++nSize)
+					{
+						DMibTestCategory("{} bytes"_f << nSize)
+						{
+							uint8 Data[16];
+							for (aint i = 0; i < nSize; ++i)
+								Data[i] = (uint8)(i * 17);
+							fTestRoundTrip(Data, nSize);
+						};
+					}
+				};
+
+				DMibTestCategory("CompressionEfficiency")
+				{
+					uint8 Data[4096];
+					for (aint i = 0; i < 4096; ++i)
+						Data[i] = 'X';
+
+					CByteVector Source = fMakeByteVector(Data, 4096);
+					CByteVector Compressed = fg_CompressHuffman(Source);
+
+					// Single-symbol data should compress significantly
+					DMibExpect(Compressed.f_GetLen(), <, Source.f_GetLen());
+
+					CByteVector Decompressed = fg_DecompressHuffman(Compressed);
+					DMibExpect(Decompressed.f_GetLen(), ==, Source.f_GetLen());
+				};
+
+				DMibTestCategory("LargeInput")
+				{
+					constexpr aint c_nSize = 32768;
+					uint8 Data[c_nSize];
+					NMib::NMisc::CRandomShiftRNG Random;
+					for (aint i = 0; i < c_nSize; ++i)
+						Data[i] = Random.f_GetValue<uint8>();
+					fTestRoundTrip(Data, c_nSize);
+				};
+
+				DMibTestCategory("RepeatedPattern")
+				{
+					uint8 Data[2048];
+					for (aint i = 0; i < 2048; ++i)
+						Data[i] = (uint8)(i % 5);
+					fTestRoundTrip(Data, 2048);
+				};
+
+				DMibTestCategory("Empty")
+				{
+					CByteVector Source;
+					CByteVector Compressed = fg_CompressHuffman(Source);
+					CByteVector Decompressed = fg_DecompressHuffman(Compressed);
+					DMibExpect(Decompressed.f_GetLen(), ==, (umint)0);
+				};
+
+				DMibTestCategory("ExplicitMaxDestinationLen")
+				{
+					uint8 Data[] = {0x10, 0x20, 0x10, 0x20, 0x10};
+					CByteVector Source = fMakeByteVector(Data, sizeof(Data));
+					CByteVector Compressed = fg_CompressHuffman(Source);
+					CByteVector Decompressed = fg_DecompressHuffman(Compressed, Source.f_GetLen());
+
+					DMibExpect(Decompressed.f_GetLen(), ==, Source.f_GetLen());
+					DMibExpect(Decompressed, ==, Source)(NMib::NTest::ETestFlag_NoValues);
+				};
+
+				DMibTestCategory("DeepTree")
+				{
+					// Fibonacci-frequency distribution for 27 symbols creates a tree of
+					// depth 26, requiring codes up to 26 bits. With 7-bit alignment offset
+					// this needs a 33-bit window, which would overflow a uint32 window.
+					constexpr aint c_nSymbols = 27;
+					int32 Freq[c_nSymbols];
+					Freq[0] = 1;
+					Freq[1] = 1;
+					for (aint i = 2; i < c_nSymbols; ++i)
+						Freq[i] = Freq[i - 1] + Freq[i - 2];
+
+					aint nTotalLen = 0;
+					for (aint i = 0; i < c_nSymbols; ++i)
+						nTotalLen += Freq[i];
+
+					CByteVector Source;
+					Source.f_SetLen(nTotalLen);
+					uint8 *pData = Source.f_GetArray();
+					aint iPos = 0;
+					for (aint i = 0; i < c_nSymbols; ++i)
+						for (int32 j = 0; j < Freq[i]; ++j)
+							pData[iPos++] = (uint8)i;
+
+					CByteVector Compressed = fg_CompressHuffman(Source);
+					CByteVector Decompressed = fg_DecompressHuffman(Compressed);
+
+					DMibExpect(Decompressed.f_GetLen(), ==, Source.f_GetLen());
+					DMibExpect(Decompressed, ==, Source)(NMib::NTest::ETestFlag_NoValues);
+				};
+
+				DMibTestCategory("MalformedInput")
+				{
+					DMibTestCategory("ShortLength")
+					{
+						uint8 Data[] = {0x01, 0x02, 0x03};
+						fExpectMalformedHuffman(Data, sizeof(Data));
+					};
+
+					DMibTestCategory("MissingNodeCount")
+					{
+						uint8 Data[] = {0x01, 0x00, 0x00, 0x00};
+						fExpectMalformedHuffman(Data, sizeof(Data));
+					};
+
+					DMibTestCategory("EmptyWithTrailingByte")
+					{
+						uint8 Data[] = {0x00, 0x00, 0x00, 0x00, 0xFF};
+						fExpectMalformedHuffman(Data, sizeof(Data));
+					};
+
+					DMibTestCategory("TruncatedNodeTable")
+					{
+						uint8 Data[] = {0x01, 0x00, 0x00, 0x00, 0x00};
+						fExpectMalformedHuffman(Data, sizeof(Data));
+					};
+
+					DMibTestCategory("InconsistentFrequencySum")
+					{
+						uint8 Data[] =
+							{
+								0x02, 0x00, 0x00, 0x00
+								, 0x00
+								, 0x01, 0x00, 0x00, 0x00
+								, 'A'
+							}
+						;
+						fExpectMalformedHuffman(Data, sizeof(Data));
+					};
+
+					DMibTestCategory("TruncatedBitstream")
+					{
+						uint8 Data[] = {0x00, 0xFF};
+						CByteVector Compressed = fg_CompressHuffman(fMakeByteVector(Data, sizeof(Data)));
+						Compressed.f_SetLen(Compressed.f_GetLen() - 1);
+
+						DMibExpectException
+							(
+								fg_DecompressHuffman(Compressed)
+								, DMibErrorInstance("Malformed Huffman compressed data")
+							)
+						;
+					};
+
+					DMibTestCategory("TrailingByte")
+					{
+						uint8 Data[] = {0x10, 0x20, 0x10, 0x20, 0x10};
+						CByteVector Compressed = fg_CompressHuffman(fMakeByteVector(Data, sizeof(Data)));
+						Compressed.f_InsertLast(uint8(0));
+						fExpectMalformedHuffman(Compressed.f_GetArray(), aint(Compressed.f_GetLen()));
+					};
+
+					DMibTestCategory("TrailingBits")
+					{
+						uint8 Data[] = {'A', 'B'};
+						CByteVector Compressed = fg_CompressHuffman(fMakeByteVector(Data, sizeof(Data)));
+						Compressed.f_GetLast() |= 0xFC;
+						fExpectMalformedHuffman(Compressed.f_GetArray(), aint(Compressed.f_GetLen()));
+					};
+
+					DMibTestCategory("DuplicateAscii")
+					{
+						// Header with two nodes sharing the same ASCII value
+						uint8 Data[] =
+							{
+								// nDesLen = 2 (little-endian uint32)
+								0x02, 0x00, 0x00, 0x00
+								// nNodes - 1 = 1 (two nodes)
+								, 0x01
+								// Node 0: frequency = 1, ascii = 'A'
+								, 0x01, 0x00, 0x00, 0x00, 'A'
+								// Node 1: frequency = 1, ascii = 'A' (duplicate)
+								, 0x01, 0x00, 0x00, 0x00, 'A'
+								// bitstream byte
+								, 0x00
+							}
+						;
+						fExpectMalformedHuffman(Data, sizeof(Data));
+					};
+
+					DMibTestCategory("ExceedsMaxDestinationLen")
+					{
+						uint8 Data[] =
+							{
+								0x20, 0x00, 0x00, 0x00
+								, 0x00
+								, 0x20, 0x00, 0x00, 0x00, 'A'
+							}
+						;
+
+						DMibExpectException
+							(
+								fg_DecompressHuffman(fMakeByteVector(Data, sizeof(Data)), 31)
+								, DMibErrorInstance("Huffman decompression output exceeds maximum buffer length")
+							)
+						;
+					};
+				};
 			};
 		}
 	};
